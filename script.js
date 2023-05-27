2f4c9e9724fbfe30f1bac566b0f54178bb275af6
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-05-27 09:34:52 -0400

    Created variables to be used for the function

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-const string = `This is a string`;
+const str = `This is a string`;
+let newArray = str.split("");
+const vowel = ['a', 'e', 'i', 'o', 'u'];
+console.log(newArray);
+let counter = 0;
