9d71ef6f6782bdf5e3a708d9728234cf3a577252
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-05-29 12:42:04 -0400

    Added comments to explain the logic of the code

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-/*const str = `This is a string`;
-let newArray = str.split("");
-const vowel = ['a', 'e', 'i', 'o', 'u'];
-//console.log(newArray);
-let counter = 0;*/
-
 findTheVowels = (str) => {
-	//let newArray = str.split("");
+	//used arrow function format to define the function
+
 	const vowel = ['a', 'e', 'i', 'o', 'u'];
+	//created a variable for the function to use to define a vowel
+
 	let counter = 0
+	//created a counter to store the number of vowels as the function iterates through the string
+
 	for (let char of str) {
 		if (vowel.includes(char)) {
 			counter ++
 		}
 	}
+	/*loops through each character of the accepted string
+	  if the array of vowels the character from a string, increase the counter by 1
+	  returns the value of the counter when the function is called
+	*/
 	return counter
 }
-console.log(findTheVowels('This sentence contains how many vowels. ' + 'Check this too.'));
+
+console.log(findTheVowels('How mamy vowels are in this question?'));
