c6f41a391c416844e98b1fe312ec7083b61574f3
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-05-29 12:25:43 -0400

    Added the loop to check for vowels

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const str = `This is a string`;
+/*const str = `This is a string`;
 let newArray = str.split("");
 const vowel = ['a', 'e', 'i', 'o', 'u'];
-console.log(newArray);
-let counter = 0;
+//console.log(newArray);
+let counter = 0;*/
+
+findTheVowels = (str) => {
+	//let newArray = str.split("");
+	const vowel = ['a', 'e', 'i', 'o', 'u'];
+	let counter = 0
+	for (let char of str) {
+		if (vowel.includes(char)) {
+			counter ++
+		}
+	}
+	return counter
+}
+console.log(findTheVowels('This sentence contains how many vowels. ' + 'Check this too.'));
